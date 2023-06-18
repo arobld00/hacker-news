25325a3ccd97717779e6a3ac86c7385320b1a7ad
arobld00 <arobld00@gmail.com>
2023-06-18 18:54:27 +0200

    eslint standard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,13 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    './node_modules/ts-standard/eslintrc.json',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
